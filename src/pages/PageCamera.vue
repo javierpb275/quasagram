1ca5fed206111682ff65aea4dee8e38ed6c82b51
javierpb275 <javierpb275@gmail.com>
2021-07-18 13:53:59 +0200

    created PageCamera.vue and added to the routes

---
+++ src/pages/PageCamera.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <h5>Camera Page</h5>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageCamera'
+})
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,8 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/PageHome.vue') }
+      { path: '', component: () => import('pages/PageHome.vue') },
+      { path: '/camera', component: () => import('pages/PageCamera.vue') }
     ]
   },
 
