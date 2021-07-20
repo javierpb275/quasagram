73ab6e444bbda28f74a73986cbf854e723ec31e8
javierpb275 <javierpb275@gmail.com>
2021-07-20 20:48:47 +0200

    created second column to the right which is the mini profile with the user image  wit its name

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,6 +1,8 @@
 <template>
   <q-page class="constrain q-pa-md">
-    <q-card v-for="post in posts" :key="post.id" class="card-post q-mb-md" flat bordered>
+    <div class="row q-col-gutter-lg">
+      <div class="col-8">
+            <q-card v-for="post in posts" :key="post.id" class="card-post q-mb-md" flat bordered>
       <q-item>
         <q-item-section avatar>
           <q-avatar>
@@ -23,6 +25,23 @@
         <div class="text-caption text-grey">{{niceDate(post.date)}}</div>
       </q-card-section>
     </q-card>
+      </div>
+      <div class="col-4">
+              <q-item class="fixed">
+        <q-item-section avatar>
+          <q-avatar size="48px">
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-bold">Pepe</q-item-label>
+          <q-item-label caption> Pepe </q-item-label>
+        </q-item-section>
+      </q-item>
+      </div>
+    </div>
+
   </q-page>
 </template>
 
