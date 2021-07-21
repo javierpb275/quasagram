2fe2ac6336c0035f27e14ea304524e4cbf15874e
javierpb275 <javierpb275@gmail.com>
2021-07-21 16:51:54 +0200

    added styles for mobile and desktop

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,15 +1,15 @@
 <template>
-  <q-page class="constrain q-pa-md">
+  <q-page class="constrain-more q-pa-md">
     <div class="camera-frame q-pa-md">
       <img class="full-width" src="https://cdn.quasar.dev/img/parallax2.jpg" />
     </div>
     <div class="text-center q-pa-md">
       <q-btn round color="grey-10" icon="eva-camera" size="lg" />
       <div class="row justify-center q-ma-md">
-        <q-input v-model="text" class="col" label="Caption" dense />
+        <q-input v-model="text" class="col col-sm-6" label="Caption" dense />
       </div>
       <div class="row justify-center q-ma-md">
-        <q-input v-model="text" class="col" label="Location" dense>
+        <q-input v-model="text" class="col col-sm-6" label="Location" dense>
           <template v-slot:append>
             <q-btn round dense flat icon="eva-navigation-2-outline" />
           </template>
