903df521f6c628afd4149e19af69cbef2167dc1d
javierpb275 <javierpb275@gmail.com>
2021-07-21 17:44:58 +0200

    working on the camera

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,10 +1,11 @@
 <template>
   <q-page class="constrain-more q-pa-md">
     <div class="camera-frame q-pa-md">
-      <img class="full-width" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+      <video v-show="!imageCaptured" ref="video" class="full-width" autoplay/>
+      <canvas v-show="imageCaptured" ref="canvas" class="full-width" height="240"></canvas>
     </div>
     <div class="text-center q-pa-md">
-      <q-btn round color="grey-10" icon="eva-camera" size="lg" />
+      <q-btn round color="grey-10" icon="eva-camera" size="lg" @click="captureImage"/>
       <div class="row justify-center q-ma-md">
         <q-input v-model="post.caption" class="col col-sm-6" label="Caption" dense />
       </div>
@@ -25,10 +26,31 @@
 <script>
 import { defineComponent } from "vue";
 import { uid } from 'quasar';
+require('md-gum-polyfill');
 
 export default defineComponent({
   name: "PageCamera",
+  preFetch() {
+    function initCamera() {
+      navigator.mediaDevices.getUserMedia({
+        video: true
+      }).then(stream => {
+        this.$refs.video.srcObject = stream;
+      })
+    }
+    initCamera()
+  },
   setup() {
+    const imageCaptured = false;
+  function captureImage() {
+      let video = this.$refs.video;
+      let canvas = this.$refs.canvas;
+      canvas.width = video.getBoundingClientRect().width;
+      canvas.height = video.getBoundingClientRect().height;
+      let context = canvas.getContext('2d');
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+      imageCaptured = true;
+    }
     return {
       post: {
         id: uid(),
@@ -36,7 +58,10 @@ export default defineComponent({
         location: '',
         photo: null,
         date: Date.now()
-      }
+      },
+      captureImage,
+      imageCaptured
+      
     }
   }
 });
