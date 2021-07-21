234c2517058375c90a393c23a6a597d8a97b7aaa
javierpb275 <javierpb275@gmail.com>
2021-07-21 16:48:46 +0200

    creating the camera page

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,13 +1,37 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Camera Page</h5>
+  <q-page class="constrain q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <img class="full-width" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+    </div>
+    <div class="text-center q-pa-md">
+      <q-btn round color="grey-10" icon="eva-camera" size="lg" />
+      <div class="row justify-center q-ma-md">
+        <q-input v-model="text" class="col" label="Caption" dense />
+      </div>
+      <div class="row justify-center q-ma-md">
+        <q-input v-model="text" class="col" label="Location" dense>
+          <template v-slot:append>
+            <q-btn round dense flat icon="eva-navigation-2-outline" />
+          </template>
+        </q-input>
+      </div>
+      <div class="row justify-center q-mt-lg">
+        <q-btn unelevated rounded color="primary" label="Post Image" />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'PageCamera'
-})
+  name: "PageCamera",
+});
 </script>
+
+<style lang="sass">
+.camera-frame
+  border: 2px solid $grey-10
+  boder-radius: 10px
+</style>
