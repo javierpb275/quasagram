2629b7c2d9f92919ec7f9b3c09d025c60266761f
javierpb275 <javierpb275@gmail.com>
2021-07-21 16:58:15 +0200

    created object post with properties for our inputs in our camrea page. imported uid from quasar to generate uid

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -6,10 +6,10 @@
     <div class="text-center q-pa-md">
       <q-btn round color="grey-10" icon="eva-camera" size="lg" />
       <div class="row justify-center q-ma-md">
-        <q-input v-model="text" class="col col-sm-6" label="Caption" dense />
+        <q-input v-model="post.caption" class="col col-sm-6" label="Caption" dense />
       </div>
       <div class="row justify-center q-ma-md">
-        <q-input v-model="text" class="col col-sm-6" label="Location" dense>
+        <q-input v-model="post.location" class="col col-sm-6" label="Location" dense>
           <template v-slot:append>
             <q-btn round dense flat icon="eva-navigation-2-outline" />
           </template>
@@ -24,9 +24,21 @@
 
 <script>
 import { defineComponent } from "vue";
+import { uid } from 'quasar';
 
 export default defineComponent({
   name: "PageCamera",
+  setup() {
+    return {
+      post: {
+        id: uid(),
+        caption: '',
+        location: '',
+        photo: null,
+        date: Date.now()
+      }
+    }
+  }
 });
 </script>
 
