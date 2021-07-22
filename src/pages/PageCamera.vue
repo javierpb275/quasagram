a0075979cfb0722dbd7539f66e2b19a18dd641b5
javier-pardinas <javierpb275@gmail.com>
2021-07-22 13:03:28 +0200

    added function to turm data url into a blob and checked if users choose or not camera support

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -5,7 +5,7 @@
       <canvas v-show="imageCaptured" ref="canvas" class="full-width" height="240"></canvas>
     </div>
     <div class="text-center q-pa-md">
-      <q-btn round color="grey-10" icon="eva-camera" size="lg" @click="captureImage"/>
+      <q-btn v-if="hasCameraSupport" round color="grey-10" icon="eva-camera" size="lg" @click="captureImage" />
       <div class="row justify-center q-ma-md">
         <q-input v-model="post.caption" class="col col-sm-6" label="Caption" dense />
       </div>
@@ -36,6 +36,8 @@ export default defineComponent({
         video: true
       }).then(stream => {
         this.$refs.video.srcObject = stream;
+      }).catch(error => {
+        this.hasCameraSupport = false;
       })
     }
     initCamera()
@@ -50,7 +52,33 @@ export default defineComponent({
       let context = canvas.getContext('2d');
       context.drawImage(video, 0, 0, canvas.width, canvas.height);
       imageCaptured = true;
+      this.post.photo = dataURItoBlob(canvas.toDataURL());
     }
+
+function dataURItoBlob(dataURI) {
+  // convert base64 to raw binary data held in a string
+  // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+  var byteString = atob(dataURI.split(',')[1]);
+
+  // separate out the mime component
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+  // write the bytes of the string to an ArrayBuffer
+  var ab = new ArrayBuffer(byteString.length);
+
+  // create a view into the buffer
+  var ia = new Uint8Array(ab);
+
+  // set the bytes of the buffer to the correct values
+  for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+  }
+
+  // write the ArrayBuffer to a blob, and you're done
+  var blob = new Blob([ab], {type: mimeString});
+  return blob;
+
+}
     return {
       post: {
         id: uid(),
@@ -60,7 +88,8 @@ export default defineComponent({
         date: Date.now()
       },
       captureImage,
-      imageCaptured
+      imageCaptured,
+      hasCameraSupport: true
       
     }
   }
