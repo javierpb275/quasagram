7da6ce9c173ea36a4a4ca938b5536ea4cbebc6a7
javier-pardinas <javierpb275@gmail.com>
2021-07-22 13:32:51 +0200

    formated document

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,21 +1,50 @@
 <template>
   <q-page class="constrain-more q-pa-md">
     <div class="camera-frame q-pa-md">
-      <video v-show="!imageCaptured" ref="video" class="full-width" autoplay/>
-      <canvas v-show="imageCaptured" ref="canvas" class="full-width" height="240"></canvas>
+      <video v-show="!imageCaptured" ref="video" class="full-width" autoplay />
+      <canvas
+        v-show="imageCaptured"
+        ref="canvas"
+        class="full-width"
+        height="240"
+      ></canvas>
     </div>
     <div class="text-center q-pa-md">
-      <q-btn v-if="hasCameraSupport" round color="grey-10" icon="eva-camera" size="lg" @click="captureImage" />
-            <q-file v-else outlined v-model="imageUpload" label="choose an image" accept="image/*" @input="captureImageFallback()">
+      <q-btn
+        v-if="hasCameraSupport"
+        round
+        color="grey-10"
+        icon="eva-camera"
+        size="lg"
+        @click="captureImage"
+      />
+      <q-file
+        v-else
+        outlined
+        v-model="imageUpload"
+        label="choose an image"
+        accept="image/*"
+        @input="captureImageFallback()"
+      >
         <template v-slot:prepend>
           <q-icon name="eva-attach-outline" />
         </template>
       </q-file>
       <div class="row justify-center q-ma-md">
-        <q-input v-model="post.caption" class="col col-sm-6" label="Caption" dense />
+        <q-input
+          v-model="post.caption"
+          class="col col-sm-6"
+          label="Caption"
+          dense
+        />
       </div>
       <div class="row justify-center q-ma-md">
-        <q-input v-model="post.location" class="col col-sm-6" label="Location" dense>
+        <q-input
+          v-model="post.location"
+          class="col col-sm-6"
+          label="Location"
+          dense
+        >
           <template v-slot:append>
             <q-btn round dense flat icon="eva-navigation-2-outline" />
           </template>
@@ -30,102 +59,102 @@
 
 <script>
 import { defineComponent } from "vue";
-import { uid } from 'quasar';
-require('md-gum-polyfill');
+import { uid } from "quasar";
+require("md-gum-polyfill");
 
 export default defineComponent({
   name: "PageCamera",
   preFetch() {
     function initCamera() {
-      navigator.mediaDevices.getUserMedia({
-        video: true
-      }).then(stream => {
-        this.$refs.video.srcObject = stream;
-      }).catch(error => {
-        this.hasCameraSupport = false;
-      })
+      navigator.mediaDevices
+        .getUserMedia({
+          video: true,
+        })
+        .then((stream) => {
+          this.$refs.video.srcObject = stream;
+        })
+        .catch((error) => {
+          this.hasCameraSupport = false;
+        });
     }
-    initCamera()
+    initCamera();
   },
   setup() {
     const imageCaptured = false;
-  function captureImage() {
+    function captureImage() {
       let video = this.$refs.video;
       let canvas = this.$refs.canvas;
       canvas.width = video.getBoundingClientRect().width;
       canvas.height = video.getBoundingClientRect().height;
-      let context = canvas.getContext('2d');
+      let context = canvas.getContext("2d");
       context.drawImage(video, 0, 0, canvas.width, canvas.height);
       imageCaptured = true;
       this.post.photo = dataURItoBlob(canvas.toDataURL());
-      disableCamera()
+      disableCamera();
     }
-function captureImageFallback(file) {
-  this.post.photo = file;
-  let canvas = this.$refs.canvas;
-  let context = canvas.getContext('2d');
-    var reader = new FileReader();
-    reader.onload = event => {
+    function captureImageFallback(file) {
+      this.post.photo = file;
+      let canvas = this.$refs.canvas;
+      let context = canvas.getContext("2d");
+      var reader = new FileReader();
+      reader.onload = (event) => {
         var img = new Image();
         img.onload = () => {
-            canvas.width = img.width;
-            canvas.height = img.height;
-            context.drawImage(img,0,0);
-            imageCaptured = true;
-        }
+          canvas.width = img.width;
+          canvas.height = img.height;
+          context.drawImage(img, 0, 0);
+          imageCaptured = true;
+        };
         img.src = event.target.result;
+      };
+      reader.readAsDataURL(file);
     }
-    reader.readAsDataURL(file);     
-  
-}
 
-function disableCamera() {
-  this.$refs.video.srcObject.getVideoTracks().forEach(track => {
-    track.stop();
-  })
-}
+    function disableCamera() {
+      this.$refs.video.srcObject.getVideoTracks().forEach((track) => {
+        track.stop();
+      });
+    }
 
-function dataURItoBlob(dataURI) {
-  // convert base64 to raw binary data held in a string
-  // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
-  var byteString = atob(dataURI.split(',')[1]);
+    function dataURItoBlob(dataURI) {
+      // convert base64 to raw binary data held in a string
+      // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+      var byteString = atob(dataURI.split(",")[1]);
 
-  // separate out the mime component
-  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+      // separate out the mime component
+      var mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
 
-  // write the bytes of the string to an ArrayBuffer
-  var ab = new ArrayBuffer(byteString.length);
+      // write the bytes of the string to an ArrayBuffer
+      var ab = new ArrayBuffer(byteString.length);
 
-  // create a view into the buffer
-  var ia = new Uint8Array(ab);
+      // create a view into the buffer
+      var ia = new Uint8Array(ab);
 
-  // set the bytes of the buffer to the correct values
-  for (var i = 0; i < byteString.length; i++) {
-      ia[i] = byteString.charCodeAt(i);
-  }
+      // set the bytes of the buffer to the correct values
+      for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+      }
 
-  // write the ArrayBuffer to a blob, and you're done
-  var blob = new Blob([ab], {type: mimeString});
-  return blob;
-
-}
+      // write the ArrayBuffer to a blob, and you're done
+      var blob = new Blob([ab], { type: mimeString });
+      return blob;
+    }
 
     return {
       post: {
         id: uid(),
-        caption: '',
-        location: '',
+        caption: "",
+        location: "",
         photo: null,
-        date: Date.now()
+        date: Date.now(),
       },
       captureImage,
       imageCaptured,
       hasCameraSupport: true,
       imageUpload: [],
-      captureImageFallback
-      
-    }
-  }
+      captureImageFallback,
+    };
+  },
 });
 </script>
 
